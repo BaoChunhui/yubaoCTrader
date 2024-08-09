fff0a07c81cd97b912d5abe4c235b596c06cd5c0
BaoChunhui <32533415+BaoChunhui@users.noreply.github.com>
2024-08-09 16:26:57 +0800

    Add files via upload

---
+++ examples/filter_pcap.py
@@ -0,0 +1,19 @@
+from yubaoCtrader.trader.utility import PcapFilter, solve_udpcap_header
+
+def callback(content, timestamp_high:int, timestamp_low:int, context):
+
+    udpcap_header, header_length = solve_udpcap_header(content)
+    
+    if udpcap_header == None:
+        return content, False
+
+    if udpcap_header.DestinationPort == 2042 or udpcap_header.DestinationPort == 4042:
+        return content, True
+    
+    else:
+        return content, False
+
+if __name__ == '__main__':
+    # f = PcapFilter("TongLian_SHFE_Level2_SHFELevel2Origin_20230522.pcapng", "", callback, None)
+    f = PcapFilter("tz_222_20240807.pcap", "A50_222_20240807.pcap", callback, None)
+    f.begin()
